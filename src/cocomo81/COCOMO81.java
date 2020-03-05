70f0964842f9570f80da7450e7b7f5aa61837d66
Cecivel <Cecivel@DESKTOP-814IUVK>
2020-03-04 22:46:47 -0500

    probando
    Conflicts:
    	src/cocomo_v1/COCOMO_v1.java

---
+++ src/cocomo81/COCOMO81.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cocomo81;
+
+/**
+ *
+ * @author Cecivel
+ */
+public class COCOMO81 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        hola como estas
+    }
+    
+}
